c9d26eeff10f9bf0937cb13ea8e1916511ad6d32
Stavya98 <joshistavya@gmail.com>
2022-10-26 08:07:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,  
+  Route,
+  Link,
+  Routes
+
+} from "react-router-dom";
+
+ 
+function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
+  return (
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Router>
+    <Navbar title="TestApp" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <div className="container">
+          <Routes>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
+            </Route>
+          </Routes>
+        </div>
+    </div>
+    </Router>
+    </> 
+  );
+}
+
+export default App;
